18eb1919152382a9b255db7cae4a3619e634cce9
Artem <spiryukov.artem@gmail.com>
2024-02-28 12:17:18 +0500

    fix old code

--- ImageTranslator.py
+++ ImageTranslator.py
@@ -14,25 +14,29 @@ def Run():
     onlyfiles = [f for f in os.listdir(SourceDirectory) if isfile(join(SourceDirectory, f))]
     for sourcefileineng in onlyfiles:
             Translate(sourcefileineng)
+
                 
 def Translate(imagefile):
     img = cv2.imread(SourceDirectory + "/" + imagefile)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Convert to HSV color-space
+    work_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    # Remove shadows, cf. https://stackoverflow.com/a/44752405/11089932
-    dilated_img = cv2.dilate(gray, np.ones((7, 7), np.uint8))
-    bg_img = cv2.medianBlur(dilated_img, 21)
-    diff_img = 255 - cv2.absdiff(gray, bg_img)
-    norm_img = cv2.normalize(diff_img, None, alpha=0, beta=255,
-                             norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+    # Up-sample
+    work_img = cv2.resize(work_img, (0, 0), fx=2, fy=2)
 
-    # Threshold using Otsu's
-    work_img = cv2.threshold(norm_img, 0, 255, cv2.THRESH_OTSU)[1]
+    # # Remove shadows, cf. https://stackoverflow.com/a/44752405/11089932
+    # dilated_img = cv2.dilate(work_img, np.ones((7, 7), np.uint8))
+    # bg_img = cv2.medianBlur(dilated_img, 21)
+    # diff_img = 255 - cv2.absdiff(work_img, bg_img)
+    # norm_img = cv2.normalize(diff_img, None, alpha=0, beta=255,
+    #                          norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
 
+    work_img = cv2.inRange(work_img, np.array([0, 0, 123]), np.array([179, 255, 255]))
     #tesseract
-    custom_config = '--psm 6'
+    config = '--oem 3 --psm %d' % 12
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    d = pytesseract.image_to_data(work_img, output_type="dict")
+    d = pytesseract.image_to_data(work_img, output_type="dict", config = config, lang='eng')
 
     #group words into phrases
     phs = {'phrases': [], 'left': [], 'top': [], 'width': [], 'height': []}
@@ -46,7 +50,7 @@ def Translate(imagefile):
              ph += d['text'][i] + " "
              left = left if left < d['left'][i] else d['left'][i]
              top = top if top < d['top'][i] else d['top'][i]
-             width += d['width'][i] +10
+             width += d['width'][i]
              height = height if height > d['height'][i] else d['height'][i]
          elif ph != '':
              phs['phrases'].append(ph) 
@@ -64,15 +68,19 @@ def Translate(imagefile):
     n_boxes = len(d['text'])
     translator = Translator()
     for i in range(len(phs['phrases'])):
-        (x, y, w, h) = (phs['left'][i], phs['top'][i], phs['width'][i], phs['height'][i])
-        ROI = img[y:y+h, x:x+w]
-        blur = cv2.GaussianBlur(ROI, (51,51), 100)
-        img[y:y+h, x:x+w] = blur
+        (x, y, w, h) = (int(phs['left'][i]), int(phs['top'][i]), int(phs['width'][i]), int(phs['height'][i]))
+        startpoint = (x, y)
+        endpoint = (x + w, y + h)
+        colorR = img[x - 1 , y - 1, 0]
+        colorG = img[x - 1 , y - 1, 1]
+        colorB = img[x - 1 , y - 1, 2]
+        img = cv2.rectangle(img, startpoint , endpoint, (int(colorR), int(colorG), int(colorB)), -1)
         trtext = translator.translate(phs['phrases'][i], dest='ru').text
-        img = cv2.putText(img, trtext, org = (phs['left'][i], phs['top'][i] + phs['height'][i]),
+        img = cv2.putText(img, trtext, org = (x, y + h),
                                              fontFace = cv2.FONT_HERSHEY_COMPLEX,
                                              fontScale = get_optimal_font_scale(phs['phrases'][i], phs['width'][i]),
-                                             color = (0, 0, 0), thickness = 1)
+                                             color = (255 - int(colorR), 255 - int(colorG), 255 - int(colorB)), 
+                                             thickness = 2)
     # for i in range(n_boxes):
     #     if int(d['conf'][i]) > 60 and regex.search('[a-zA-Z]', d['text'][i]):
     #         (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
@@ -91,7 +99,7 @@ def Translate(imagefile):
 
 def get_optimal_font_scale(text, width):
     for scale in reversed(range(0, 60, 1)):
-        textSize = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=scale/10, thickness=1)
+        textSize = cv2.getTextSize(text, fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=scale/10, thickness=2)
         new_width = textSize[0][0]
         if (new_width <= width):
             print(new_width)
